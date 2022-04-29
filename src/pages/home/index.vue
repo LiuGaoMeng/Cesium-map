22a5d5eacf4ef1a14522ffb304b384886a687859
lgm <lgm123589@gmail.com>
2022-04-29 17:02:26 +0800

    fix(demo): demo

---
+++ src/pages/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <router-view></router-view>
+        <MapDemo/>
+    </div>
+</template>
+
+<script>
+import MapDemo from './component/map-demo'
+
+export default {
+    data() {
+        return {}
+    },
+    mounted() {
+
+    },
+    components: {
+        MapDemo
+    }
+}
+</script>
